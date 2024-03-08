934dc34b079616266e8ea85f98551370bd629562
Paulien <paulien.h.7@gmail.com>
2024-03-07 20:08:27 -0500

    nativehtmlsolutions>>>>

--- gameplank.js
+++ gameplank.js
@@ -1,15 +1,19 @@
-let canvas = document.getElementById("canvas");
-let context = canvas.getContext("2d");
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
 
-canvas.width = window.innerWidth - 40;
-canvas.height = window.innerHeight - 50;
 
-canvas.style.border = '5px solid red';
 
-let canvas_width = canvas.width;
-let canvas_height = canvas.height;
 
-let shapes = [];
-shapes.push( {x: 0, y: 0, width: 200, height: 200, color: 'blue'});
-shapes.push( {x: 0, y: 0, width: 100, height: 100, color: 'orange'});
+
 
